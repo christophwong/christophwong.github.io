43fdb564a934939a8c09f6662948d9a0ef3f9b3a
Christoph Wong <wong.christoph@gmail.com>
2018-11-29 13:31:34 -0800

    add js to make section nav more dynamic

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const hideSection = (sectionId) => {
+  const section = document.querySelector(sectionId);
+  section.hidden = true;
+}
+
+const showSection = sectionId => {
+  let sectionElement = document.querySelector(sectionId);
+    sectionElement.hidden = false;
+}
+
+const hideElementWithChecks = prevElementId => {
+ if (prevElementId.length !== 0){
+  hideSection(prevElementId);
+}
+}
+
+// Looping over each Nav Item / Section. 
+const navMenu = document.querySelector('ul.menu');
+const navLength = navMenu.children.length;
+let prevElementId = '';
+for (let i = 0; i < navLength; i++){
+  let anchorLink = navMenu.children[i].firstChild;
+  let sectionId = anchorLink.attributes.href.value;
+  //hide each section when first loaded to leave behind header only.
+  hideSection(sectionId);
+// Adding a Click event listener onto AnchorLink, most logic goes in here.
+  anchorLink.addEventListener('click', () => {
+    hideElementWithChecks(prevElementId);
+    prevElementId = sectionId;
+    showSection(sectionId);
+  })
+}
